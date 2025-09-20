c78d2d908333ce0bcb89c54e8d5e8d3658d39ea0
Ankit Pathak <ankit.pathak@optimusinfo.com>
2025-09-20 18:24:11 +0530

    FIX: Simplified VM Extension to use direct commandToExecute
    - Changed from protected_settings.script to settings.commandToExecute
    - Removed base64 encoding complexity that was causing issues
    - Direct template execution should be more reliable
    - This approach is simpler and commonly used for Azure CustomScript extensions

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -13,8 +13,3 @@ output "vm_private_ip" {
   description = "Private IP address of the virtual machine"
   value       = azurerm_linux_virtual_machine.vm.private_ip_address
 }
-
-output "vm_public_ip" {
-  description = "Public IP address of the virtual machine"
-  value       = azurerm_linux_virtual_machine.vm.public_ip_address
-}
